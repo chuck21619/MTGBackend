e46edfd5223fed80bd9549abc1d4855d6a5515f8
Charles Johnston <charles.ward.johnston@gmail.com>
2025-04-25 19:31:39 -0400

    uncomment login routing

--- backend/main.go
+++ backend/main.go
@@ -85,7 +85,7 @@ func main() {
 
 
 	http.HandleFunc("/register", registerHandler)
-	//http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/login", loginHandler)
 
 	log.Println("Listening on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
